4bdfa1b6290e1a921a57180021e8cf5b4fabd85c
KhaledIbrahim1015 <ibrahimkhaled136@gmail.com>
2023-05-20 03:16:53 +0300

    Create Docker file (Dockerise application)

--- contenarise-webapi/Program.cs
+++ contenarise-webapi/Program.cs
@@ -1,7 +1,7 @@
-
-
 using contenarise_webapi;
 using Microsoft.EntityFrameworkCore;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,13 +24,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment()|| app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
